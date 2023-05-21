7280a8ed4d015ad998bd7689d2cb44964d53f954
SuperSecureHuman <supersecurehuman@gmail.com>
2023-05-21 10:47:57 +0530

    Smallest

---
+++ src/worksheet_1/SmallestValues.scala
@@ -0,0 +1,28 @@
+package worksheet_1
+
+object SumSmallestArray {
+
+  def main(args: Array[String]): Unit = {
+    var test = Array(1, 2, 7, 2, 8, 3, 6, 8, 9)
+
+    var Smallest = Int.MaxValue
+    var SecondSmallest = Int.MaxValue
+    var ThirdSmallest = Int.MaxValue
+
+    for (i <- test.indices) {
+      if (test(i) < Smallest) {
+        ThirdSmallest = SecondSmallest
+        SecondSmallest = Smallest
+        Smallest = test(i)
+      }
+      else if (test(i) < SecondSmallest) {
+        ThirdSmallest = SecondSmallest
+        SecondSmallest = test(i)
+      }
+      else if (test(i) < ThirdSmallest) {
+        ThirdSmallest = test(i)
+      }
+    }
+    println(s"$Smallest $SecondSmallest $ThirdSmallest")
+  }
+}
